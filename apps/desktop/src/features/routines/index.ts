ee6cc13cfb65f92bc9a2abd0823301f84abb4f34
venmak.li <kamnevin@gmail.com>
2026-08-08 16:20:45 +0700

    Tighten routines public API

--- apps/desktop/src/features/routines/index.ts
+++ apps/desktop/src/features/routines/index.ts
@@ -1,7 +1,1 @@
 export { RoutinesSurface } from "./ui/routines-surface";
-export type {
-  RoutineCatalogSnapshot,
-  RoutineDefinition,
-  RoutineRow,
-  RoutineSessionTarget,
-} from "./model/types";
